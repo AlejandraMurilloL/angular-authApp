b52f84ea3a06f529612ed10fd5f679e10d3ae125
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-08-11 22:09:35 -0500

    Se implementan las rutas con lazy load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth', loadChildren: () => import('./auth/auth-routing.module').then(m => m.AuthRoutingModule)
+  },
+  {
+    path: 'dashboard', loadChildren: () => import('./protected/protected-routing.module').then(m => m.ProtectedRoutingModule)
+  },
+  {
+    path: '**', redirectTo: 'auth'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
